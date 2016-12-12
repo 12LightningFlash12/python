78d34cbb432ac4852ceed2422d85e9e52d3c5e3a
LightningFlash <12lightningflash12@gmail.com>
2016-12-12 13:24:37 -0500

    Update brony-test.py

--- brony-test.py
+++ brony-test.py
@@ -20,6 +20,7 @@ else:
 
 print '\n'
 
+
 q2 = raw_input('Who voice acts Princess Luna? ')
 
 if q2 == 'Tabitha St. Germain':
@@ -35,19 +36,49 @@ else:
 
 print '\n'
 
+
+q3 = raw_input('What was the original release date for MLP FIM season one? (use mm/dd/yy format) ');
+
+if q3 == '10/10/10':
+    print 'Correct\n'
+    POINTS += 1
+    print 'Points: '
+    print POINTS
+else:
+    print 'Wrong\n'
+    POINTS -= 1
+    print 'Points: '
+    print POINTS
+
+print '\n'
+
+
+q4 = raw_input("What is the name of Rarity's boulder")
+
+if q4 == 'Tom' or q4 == 'tom':
+    print 'Correct\n'
+    POINTS += 1
+    print 'Points: '
+    print POINTS
+else:
+    print 'Wrong\n'
+    POINTS -= 1
+    print 'Points: '
+    print POINTS
+
+print '\n'
+
+
 print 'Your final points are: '
 print POINTS
 
-'''
-grading down below
-i have not figured out the comments
-'''
+print '\n'
 
 if POINTS <= 20 and POINTS >= 16:
     print ''
-else if POINTS <= 15 and POINTS >= 11:
+elif POINTS <= 15 and POINTS >= 11:
     print ''
-else if POINTS <= 10 and POINTS >=6:
+elif POINTS <= 10 and POINTS >=6:
     print ''
 else:
     print ''
